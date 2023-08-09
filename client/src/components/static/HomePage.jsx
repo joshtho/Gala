9299f51001e91d9a3ee45e3b1ba1c3e60c2ba2ad
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-08-09 16:06:50 -0500

    update HomePage

--- client/src/components/static/HomePage.jsx
+++ client/src/components/static/HomePage.jsx
@@ -29,7 +29,7 @@ function HomePage() {
           <img alt='Home-image' src='https://media.istockphoto.com/id/1218961153/photo/art-museum.jpg?s=612x612&w=0&k=20&c=9fK54fu1mjzFjDOSqg_jfrMy4Hkp8vsmImB7rLrbhJs='></img>
           <Container>
           <br></br>
-          <p className='paragraph' >Welcome to Galla! Galla is a site to store information about your favorite artists and their art pieces. After signup, use the Artists tab to find and save artists with an artwork that other users have added. Use the My Artists tab to see artists and their artwork that you have saved. All artwork saved will be loaded onto this homepage. Enjoy!</p>
+          <p className='paragraph' >Welcome to Galla! Galla is a site to store information about your favorite artists and their art pieces. After signup, use the Artists tab to find and save artists that other users have added. Use the My Artists tab to see artists and their artwork that you have saved. All artwork saved will be loaded onto this homepage upon login. Enjoy!</p>
           </Container>
 
           </div>
