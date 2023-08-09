a2d19460809a5e067dbc741f8431daabf40b89f3
Joshua Keane Thompson <joshua.thompson0010@gmail.com>
2023-08-09 13:36:30 -0500

    ArtistList cleanup

--- client/src/components/artists/ArtistList.jsx
+++ client/src/components/artists/ArtistList.jsx
@@ -1,43 +1,27 @@
-import React, { useEffect, useState } from 'react'
-import { useDispatch, useSelector } from 'react-redux'
+import React, { useState } from 'react'
+import { useSelector } from 'react-redux'
 import ArtistTile from './ArtistTile'
 import { Link, useNavigate } from 'react-router-dom'
 import Button from 'react-bootstrap/esm/Button'
 import Container from 'react-bootstrap/esm/Container'
-import { addNewArtist, resetArtistObj } from '../../features/artistsSlice'
-import { addArtistToUser } from '../../features/sessionSlice'
 
 function ArtistList() {
-  // const dispatch = useDispatch()
   const navigate = useNavigate()
-  const artists = useSelector(state => state.artists.entities)
-  
-  // const [showButton, setShowButton] = useState(false)
-  const newArtist = useSelector(state => state.artists.artistObj)
+  const artists = useSelector(state => state.artists.entities)  
   
   const initialObj = {
     name: '',
     description: '',
     image: '',
   }
-    
   const [newArtistData, setNewArtistData] = useState(initialObj)
-
+    
   function handleNewArtistClick(id) {
     navigate(`/artworks/add/${id}`)
-    
   }
     
-  // useEffect(() => {
-  //   if (newArtist) {
-  //     dispatch(addArtistToUser(newArtist))
-  //     dispatch(resetArtistObj())
-  //     setNewArtistData(initialObj)
-  //   }
-  // },[newArtist])
-
   return (
-    <div  >
+    <div >
       <h1>Choose an Artist</h1>
       <Container className='card-grid'>
         { artists ?
